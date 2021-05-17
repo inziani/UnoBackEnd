cf79797eb27447af331564cd8c078b2f386e225c
“inziani” <ValentineRobai.Inziani@gmail.com>
2021-05-17 16:13:15 +0300

    Add JWT Authentication

--- onlineShop/urls.py
+++ onlineShop/urls.py
@@ -20,6 +20,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 from rest_framework import routers
+# from rest_framework_simplejwt.views  import (TokenObtainPairView, TokenRefreshView, )
 
 
 
@@ -44,7 +45,9 @@ router.extend(activitys_router)
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_views, name='token_refresh'),
+    path('', include(router.urls)),
 
 ]
--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
 # Create your models here.
 
 #import _typeshed
+import rest_framework_jwt
+from django.conf import settings
+from datetime import datetime, timedelta
 from django.db import models
 from django.core.mail import send_mail
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, UserManager
@@ -27,8 +30,10 @@ class User(AbstractBaseUser, PermissionsMixin):
   city = models.CharField(verbose_name= _("City"), blank=True, null=True, max_length=255)
   country = CountryField(multiple=True,  blank_label='(select country)', default='')
   is_active = models.BooleanField(default=True)
+  # The is_active flag offers users a chance to deactivate their account as opposed to deletion which completely removes a users account from the application.
   is_superuser = models.BooleanField(default=False)
   is_staff = models.BooleanField(default=False)
+  # The is_staff flag is expected by django to determine who can and cannot log on to the admin site. The flag will be active for staff only
   date_joined = models.DateTimeField(default=timezone.now)
 
   objects = UserManager()
@@ -44,11 +49,35 @@ class User(AbstractBaseUser, PermissionsMixin):
     return f'{self.first_name} + " " {self.last_name}'
 
   def __str__(self):
-        return self.email
+    """ Returns the string representation os this 'User'. This string is used when a 'User' is printed in the console"""
+    return self.email
 
   def email_user(self,subject, message, from_email = None, **kwargs):
     "Sends and email to this user"
     send_mail( subject, message, from_email, [self.email], **kwargs)
+  
+  @property
+  def token(self):
+    """
+    Allows us to get a user's token by calling 'user.token' instead of 'user.generate_jwt_token()
+    The @property makes it a class property
+    """
+    return self.generate_jwt_token()
+
+  def generate_jwt_token(self):
+    """
+    Generate a JSON Web Token that stores the user's ID, and has an expiry date of 60 days into the future
+
+    """
+    dt = datetime.now() + timedelta(days=60)
+    token = rest_framework_jwt.jwt.encode({
+      'id': self.pk,
+      'exp': int(dt.strftime('%s'))
+    }, 
+    settings.SECRET_KEY, algorithm='HS256')
+    return token.decode('utf-8')
+
+
   
 
 class UserProfile(models.Model):
